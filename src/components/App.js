278a87b76aa6b2e39bf6d02f12e5bb70ec8f434d
Anja Seeba <ahoi@achtsam.media>
2021-02-22 20:34:25 +0100

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import './App.css'
 import PlayerForm from './PlayerForm'
+import Header from './Header'
 
 function App() {
   return (
+    <>
+    <Header title="SCOREKEEPER" />
     <div className="App">
       <PlayerForm onAddPlayer={() => console.log('onAddPlayer')} />
     </div>
+    </>
   )
 }
 
